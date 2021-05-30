e799f693a5fe4126f8a92e912041b8fc9c183558
Manuel <giudicimanuel@gmail.com>
2021-05-30 15:40:58 -0300

    creo modelos

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class);
+    }
+
+    public function size()
+    {
+        return $this->hasMany(Size::class);
+    }
+
+    public function image()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}

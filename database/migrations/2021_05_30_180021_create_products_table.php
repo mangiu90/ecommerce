74f102a9532295cdc8decae06c2b04cd595a3895
Manuel <giudicimanuel@gmail.com>
2021-05-30 17:45:04 -0300

    modifico migraciones

--- database/migrations/2021_05_30_180021_create_products_table.php
+++ database/migrations/2021_05_30_180021_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -29,7 +30,9 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('brand_id');
             $table->foreign('brand_id')->references('id')->on('brands');
 
-            $table->integer('quantity');
+            $table->integer('quantity')->nullable();
+
+            $table->enum('status', [Product::BORRADOR, Product::PUBLICADO])->default(Product::BORRADOR);
 
             $table->timestamps();
         });

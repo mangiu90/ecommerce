2dde69f19b18d083147b4ba32ea096127d71ba11
Manuel <giudicimanuel@gmail.com>
2021-05-30 17:50:18 -0300

    creo seeders y factories

---
+++ database/seeders/SizeSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Builder;
+
+class SizeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = Product::whereHas('subcategory', function (Builder $query) {
+            $query->where('color', true)->where('size', true);
+        })->get();
+
+        $sizes = ['S', 'M', 'L'];
+
+        foreach ($products as $product) {
+
+            foreach ($sizes as $size) {
+                $product->sizes()->create([
+                    'name' => $size
+                ]);
+            }
+        }
+    }
+}
---
+++ database/seeders/SubcategorySeeder.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subcategory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class SubcategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $subcategories = [
+            [
+                'category_id' => 1,
+                'name' => 'Celulares y smartphones',
+                'slug' => Str::slug('Celulares y smartphones'),
+                'color' => true
+            ],
+            [
+                'category_id' => 1,
+                'name' => 'Accesorios para celulares',
+                'slug' => Str::slug('Accesorios para celulares'),
+            ],
+            [
+                'category_id' => 1,
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'TV y audio',
+                'slug' => Str::slug('TV y audio'),
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'Audios',
+                'slug' => Str::slug('Audios'),
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'Audio para auto',
+                'slug' => Str::slug('Audio para auto'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Xbox',
+                'slug' => Str::slug('Xbox'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Play Station',
+                'slug' => Str::slug('Play Station'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Nintendo Switch',
+                'slug' => Str::slug('Nintendo Switch'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Videojuegos para PC',
+                'slug' => Str::slug('Videojuegos para PC'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Portatiles',
+                'slug' => Str::slug('Portatiles'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Pc escritorio',
+                'slug' => Str::slug('Pc escritorio'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Almacenamiento',
+                'slug' => Str::slug('Almacenamiento'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Accesorios computadoras',
+                'slug' => Str::slug('Accesorios computadoras'),
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Mujeres',
+                'slug' => Str::slug('Mujeres'),
+                'color' => true,
+                'size' => true,
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Hombres',
+                'slug' => Str::slug('Hombres'),
+                'color' => true,
+                'size' => true,
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Lentes',
+                'slug' => Str::slug('Lentes'),
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Relojes',
+                'slug' => Str::slug('Relojes'),
+            ],
+        ];
+
+        foreach ($subcategories as $subcategory) {
+            Subcategory::factory(1)->create($subcategory);
+        }
+    }
+}

730acd732ace0fd522d6c4be387420574d26bc43
Manuel <giudicimanuel@gmail.com>
2021-07-07 21:47:04 -0300

    paypal parcial

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -39,103 +39,111 @@
 
     @endphp
 
-    <div class="container py-8">
-        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
-            <p class="text-gray-700 uppercase"><span class="font-semibold">Número de orden: </span>Orden-{{$order->id}}</p>
-        </div>
+    <div class="container py-8 grid grid-cols-5 gap-6">
+        <div class="col-span-3">
+            <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+                <p class="text-gray-700 uppercase"><span class="font-semibold">Número de orden: </span>Orden-{{$order->id}}</p>
+            </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
-            <div class="grid grid-cols-2 gap-6 text-gray-700">
-                <div>
-                    <p class="text-lg font-semibold uppercase">Envío</p>
+            <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
+                <div class="grid grid-cols-2 gap-6 text-gray-700">
+                    <div>
+                        <p class="text-lg font-semibold uppercase">Envío</p>
 
-                    @if ($order->envio_type = 1)
-                        <p class="text-sm">Los productos se retiran en tienda</p>
-                        <p class="text-sm">Calle falsa 123</p>
-                    @else
-                        <p class="text-sm">Los productos se envian a:</p>
-                        <p class="text-sm">{{$order->address}}</p>
-                        <p class="">{{$order->city->name}} - {{$order->city->province->name}} - {{$order->city->province->country->name}}</p>
-                    @endif
+                        @if ($order->envio_type = 1)
+                            <p class="text-sm">Los productos se retiran en tienda</p>
+                            <p class="text-sm">Calle falsa 123</p>
+                        @else
+                            <p class="text-sm">Los productos se envian a:</p>
+                            <p class="text-sm">{{$order->address}}</p>
+                            <p class="">{{$order->city->name}} - {{$order->city->province->name}} - {{$order->city->province->country->name}}</p>
+                        @endif
+                    </div>
+
+                    <div>
+                        <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+
+                        <p class="text-sm">Persona que recibe el envío: {{$order->contact}}</p>
+                        <p class="text-sm">Teléfono de contacto: {{$order->phone}}</p>
+                    </div>
                 </div>
+            </div>
 
-                <div>
-                    <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+            <div class="bg-white rounded-lg shadow-lg p-6 text-gray-700 mb-6">
+                <p class="text-xl font-semibold mb-4">Resumen</p>
 
-                    <p class="text-sm">Persona que recibe el envío: {{$order->contact}}</p>
-                    <p class="text-sm">Teléfono de contacto: {{$order->phone}}</p>
-                </div>
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Precio</th>
+                            <th>Cant</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($items as $item)
+                            <tr>
+                                <td>
+                                    <div class="flex">
+                                        <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+
+                                        <article>
+                                            <h1 class="font-bold">{{ $item->name }}</h1>
+                                            <div class="flex text-xs">
+                                                @isset ($item->options->color)
+                                                    Color: {{ __($item->options->color) }}
+                                                @endif
+
+                                                @isset ($item->options->size)
+                                                    Talle: {{ $item->options->size }}
+                                                @endif
+                                            </div>
+                                        </article>
+                                    </div>
+                                </td>
+                                <td class="text-center">
+                                    {{ $item->price }} USD
+                                </td>
+                                <td class="text-center">
+                                    {{ $item->qty }}
+                                </td>
+                                <td class="text-center">
+                                    {{ $item->price * $item->qty}} USD
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 text-gray-700 mb-6">
-            <p class="text-xl font-semibold mb-4">Resumen</p>
+        <div class="col-span-2">
+            <div class="bg-white rounded-lg shadow-lg p-6">
+                <div class="flex justify-between items-center mb-6">
+                    <img class="h-8" src="{{ asset('img/tarjetas.png') }}" alt="">
 
-            <table class="table-auto w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Precio</th>
-                        <th>Cant</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
+                    <div class="text-gray-700">
+                        <p class="text-sm font-semibold">
+                            Subtotal: {{ $order->total - $order->shipping_cost }} USD
+                        </p>
 
-                <tbody>
-                    @foreach ($items as $item)
-                        <tr>
-                            <td>
-                                <div class="flex">
-                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                        <p class="text-sm font-semibold">
+                            Envío: {{ $order->shipping_cost }} USD
+                        </p>
 
-                                    <article>
-                                        <h1 class="font-bold">{{ $item->name }}</h1>
-                                        <div class="flex text-xs">
-                                            @isset ($item->options->color)
-                                                Color: {{ __($item->options->color) }}
-                                            @endif
-
-                                            @isset ($item->options->size)
-                                                Talle: {{ $item->options->size }}
-                                            @endif
-                                        </div>
-                                    </article>
-                                </div>
-                            </td>
-                            <td class="text-center">
-                                {{ $item->price }} USD
-                            </td>
-                            <td class="text-center">
-                                {{ $item->qty }}
-                            </td>
-                            <td class="text-center">
-                                {{ $item->price * $item->qty}} USD
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        <div class="bg-white rounded-lg shadow-lg p-6 flex justify-between items-center">
-            <img class="h-8" src="{{ asset('img/tarjetas.png') }}" alt="">
-
-            <div class="text-gray-700">
-                <p class="text-sm font-semibold">
-                    Subtotal: {{ $order->total - $order->shipping_cost }} USD
-                </p>
-
-                <p class="text-sm font-semibold">
-                    Envío: {{ $order->shipping_cost }} USD
-                </p>
-
-                <p class="text-lg font-semibold uppercase">
-                    Total: {{ $order->total }} USD
-                </p>
-
-                <div class="cho-container">
-
+                        <p class="text-lg font-semibold uppercase">
+                            Total: {{ $order->total }} USD
+                        </p>
+                    </div>
                 </div>
+
+                <div class="flex items-center justify-center mb-3">
+                    <div class="cho-container"></div>
+                </div>
+                
+                {{-- <div id="paypal-button-container"></div> --}}
             </div>
         </div>
     </div>
@@ -155,9 +163,34 @@
               },
               render: {
                     container: '.cho-container', // Indica dónde se mostrará el botón de pago
-                    label: 'Pagar', // Cambia el texto del botón de pago (opcional)
+                    label: 'Pagar con Mercado Pago', // Cambia el texto del botón de pago (opcional)
               }
         });
     </script>
+
+{{--     <script src="https://www.paypal.com/sdk/js?&client-id={{config('services.paypal.client_id')}}"></script>
+
+    <script>
+        paypal.Buttons({
+            createOrder: function (data, actions) {
+                return fetch('/my-server/create-order', {
+                    method: 'POST'
+                }).then(function(res) {
+                    return res.json();
+                }).then(function(data) {
+                    return data.id;
+                });
+            },
+            onApprove: function (data, actions) {
+                return fetch('/my-server/capture-order/' + data.orderID, {
+                    method: 'POST'
+                }).then(function(res) {
+                    if (!res.ok) {
+                    alert('Something went wrong');
+                    }
+                });
+            }
+        }).render('#paypal-button-container');
+    </script> --}}
 
 </x-app-layout>

21826c234f94b05c2e2a00c85564257c68216f97
Manuel <giudicimanuel@gmail.com>
2021-08-02 13:11:44 -0300

    banner pendientes

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
--- routes/web.php
+++ routes/web.php
@@ -34,12 +34,12 @@ Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
 
 Route::middleware(['auth'])->group(function(){
     Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
-    Route::get('orders/create', CreateOrder::class)->middleware('auth')->name('orders.create');
-    Route::get('orders/{order}', [OrderController::class, 'show'])->middleware('auth')->name('orders.show');
-    Route::get('orders/{order}/payment', PaymentOrder::class)->middleware('auth')->name('orders.payment');
+    Route::get('orders/create', CreateOrder::class)->name('orders.create');
+    Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+    Route::get('orders/{order}/payment', PaymentOrder::class)->name('orders.payment');
     
     
-    Route::get('orders/{order}/pay', [OrderController::class, 'pay'])->middleware('auth')->name('orders.pay');
+    Route::get('orders/{order}/pay', [OrderController::class, 'pay'])->name('orders.pay');
     
     Route::post('webhooks', WebhooksController::class);
 });
